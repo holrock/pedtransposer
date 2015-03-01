e6fc9e8fcb23686fc804d6819dded0232e0df1b7
holrock <ispeporez@gmail.com>
2015-03-01 19:28:34 +0900

    set buf size from file size

--- main.c
+++ main.c
@@ -3,8 +3,61 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 const int N_COL_HEADER = 6;
+
+struct Opt
+{
+  const char* file_name;
+  size_t buf_size;
+};
+
+struct Buf
+{
+  char* data;
+  size_t size;
+};
+
+
+static
+unsigned long xstrtoul(const char* s)
+{
+  char* endp = NULL;
+  errno = 0;
+  unsigned long val = strtoul(s, &endp, 10);
+  if ((errno == ERANGE && (val == ULONG_MAX || val == 0)) || (errno != 0 && val == 0)) {
+    perror("strtoul");
+    exit(-1);
+  }
+
+  if (endp == s) {
+    fprintf(stderr, "strtoul error: %s\n", s);
+    exit(-1);
+  }
+  return val;
+}
+
+static
+size_t get_file_size(const char* s)
+{
+  struct stat st = {};
+  if (stat(s, &st) == -1) {
+    perror("stat");
+    exit(-1);
+  }
+  if (st.st_size < 1) {
+    fprintf(stderr, "invalid file size\n");
+    exit(-1);
+  }
+  return (size_t)st.st_size;
+}
 
 static inline
 bool is_eos(char c)
@@ -84,7 +137,7 @@ static
 void transpose(char* buf, FILE* out, char delim)
 {
   if (!buf || buf[0] == '\0') {
-    fprintf(stderr, "buffer empty");
+    fprintf(stderr, "invalid buffer\n");
     return;
   }
 
@@ -97,37 +150,78 @@ void transpose(char* buf, FILE* out, char delim)
   }
 }
 
-int main(int argc, char** argv)
+static
+struct Buf init_buf(const char* file_name, size_t buf_size)
 {
-  if (argc < 2)
-    return -1;
-
-  size_t buf_size = 1024 * 1024 * 1024; // 1GiB
-  char* buf = malloc(buf_size);
-  if (!buf) {
-    fprintf(stderr, "malloc error");
-    return -1;
+  struct Buf buf = {};
+  if (buf_size != 0) {
+    buf.data = malloc(buf_size);
+    buf.size = buf_size;
+  } else {
+    size_t size = get_file_size(file_name) + 1;
+    buf.data = malloc(size);
+    buf.size = size;
+    fprintf(stderr, "buf isze %lu\n", buf.size);
   }
 
-  char* ped = argv[1];
-  FILE* fp = fopen(ped, "rb");
+  if (!buf.data) {
+    fprintf(stderr, "malloc error\n");
+    exit(-1);
+  }
+  return buf;
+}
+
+static
+int start_transpose(const char* file_name, struct Buf buf)
+{
+  FILE* fp = fopen(file_name, "rb");
   if (!fp) {
-    perror(ped);
+    perror("fopen");
     return -1;
   }
 
   while (!feof(fp)) {
-    size_t n = fread(buf, sizeof(char), buf_size-1, fp);
+    size_t n = fread(buf.data, sizeof(char), buf.size, fp);
     if (ferror(fp)) {
-      perror(ped);
+      perror("fread");
+      fclose(fp);
       return -1;
     }
+    fprintf(stderr, "read isze %lu\n", n);
 
-    buf[n + 1] = '\0';
-    transpose(buf, stdout, '\t');
+    buf.data[n + 1] = '\0';
+    transpose(buf.data, stdout, '\t');
   }
-
-  free(buf);
   fclose(fp);
   return 0;
+}
+
+static
+struct Opt parse_opt(int argc, char** argv)
+{
+  struct Opt opt = {};
+
+  for (int i = 1; i < argc - 1; ++i) {
+    if (strcmp(argv[i], "--file") == 0) {
+      opt.file_name = argv[i + 1];
+      ++i;
+    } else if (strcmp(argv[i], "--buf") == 0) {
+      opt.buf_size = xstrtoul(argv[i + 1]);
+      ++i;
+    }
+  }
+  if (!opt.file_name) {
+    fprintf(stderr, "empty file name\n");
+    exit(-1);
+  }
+  return opt;
+}
+
+int main(int argc, char** argv)
+{
+  struct Opt opt = parse_opt(argc, argv);
+  struct Buf buf = init_buf(opt.file_name, opt.buf_size);
+  int status = start_transpose(opt.file_name, buf);
+  free(buf.data);
+  return status;
 }
